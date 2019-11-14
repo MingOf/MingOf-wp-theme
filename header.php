f7ca2521203f4bf788610246c1e3956e7fa1f610
mingof <linyongqiang@outlook.com>
2019-11-14 16:09:29 +0800

    bugs fixed

--- header.php
+++ header.php
@@ -8,18 +8,18 @@
     <meta name="description" content="<?php bloginfo("description"); ?>">
     <title><?php bloginfo("name");?></title>
     <?php wp_head(); ?>
+    <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
     <header id="header">
         <div id="headering">
             <div id="logo"><?php if(has_custom_logo()) { the_custom_logo();} else { echo '<img src='.'"'.get_template_directory_uri().'/assets/image/logo.jpg'.'"'.'alt="logo"/>';}?></div>
-<!--            <h1><a href="--><?php //echo get_option('home');?><!--">--><?php //bloginfo('name');?><!--</a></h1>-->
-            <h1 id="name"><a href="<?php echo get_option('home');?>">MingOf</a></h1>
+            <h1 id="name"><a href="<?php echo get_option('home');?>"><?php bloginfo('name');?></a></h1>
+<!--            <h1 id="name"><a href="--><?php //echo get_option('home');?><!--">MingOf 前端日志</a></h1>-->
             <div class="description"><?php bloginfo("description");?></div>
             <div class="nav-toggle nav-close"><span></span></div>
         </div>
         <nav id="nav"><?php wp_nav_menu()?></nav>
-<!--        --><?php //get_sidebar();?>
         <?php get_template_part('footer');?>
     </header>
 
