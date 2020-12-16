54332a219d4ab5e97fac3cf8036ab1a0bcff7e1b
johnny <linyongqiang@outlook.com>
2020-12-16 22:35:19 +0800

    移动端 banner 支持文章thumbnail

--- banner-m.php
+++ banner-m.php
@@ -1,4 +1,10 @@
-<div id="banner">
+<div id="banner"  style="background-image: url('<?php
+if(has_post_thumbnail()) {
+    echo wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'thumbnail')[0];
+} else {
+    echo get_template_directory_uri().'/assets/image/banner.jpg';
+}
+?>')">
     <div id="banner-headering">
         <h1 id="banner-name"><a href="<?php echo get_option('home');?>"><?php bloginfo('name');?></a></h1>
     </div>
