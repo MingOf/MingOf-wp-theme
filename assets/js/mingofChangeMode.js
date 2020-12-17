74eaa91211085329f74df0869d0bd88770a9ebd7
johnny <linyongqiang@outlook.com>
2020-12-17 09:22:51 +0800

    增强夜间模式检测

--- assets/js/mingofChangeMode.js
+++ assets/js/mingofChangeMode.js
@@ -2,6 +2,7 @@
 * 检测夜间模式
 * */
 (function () {
+    let html = document.documentElement;
     function isMobile(){
         let info = navigator.userAgent;
         let agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPod", "iPad"];
@@ -10,21 +11,35 @@
         }
         return false;
     }
+    function getRGB(str){
+        let match = str.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+        return match ? {
+            r: match[1],
+            g: match[2],
+            b: match[3]
+        } : {};
+    }
     function checkDarkMode () {
         let matches = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark').matches;
-        if(matches) {
-            document.getElementsByTagName("html")[0].classList.add('dark');
+        if(matches) return matches;
+        let color = window.getComputedStyle(document.documentElement).backgroundColor;
+        color = getRGB(color);
+        if(color.r*0.299 + color.g*0.578 + color.b*0.114 >= 192){ //浅色
+            matches = false;
+        }else{  //深色
+            matches = true;
         }
+        return matches;
     }
-    function unCheckDarkMode () {
-        document.getElementsByTagName("html")[0].classList.remove('dark');
+    function changeMode (el, isDark) {
+        isDark ? el.classList.add("dark") : el.classList.remove("dark");
     }
     window.matchMedia('(prefers-color-scheme: dark').addEventListener('change', (event) => {
         if(event.matches) {
-            checkDarkMode();
+            changeMode(html, true);
         } else {
-            unCheckDarkMode();
+            changeMode(html, false);
         }
     })
-    checkDarkMode();
-})()
+    changeMode(html, checkDarkMode());
+})();
