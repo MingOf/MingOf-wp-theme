3fa94973cc6b7386453407a578b596195c747e2a
johnny <linyongqiang@outlook.com>
2020-12-26 16:56:03 +0800

    goTop

--- templates/go-top.php
+++ templates/go-top.php
@@ -1,4 +1,6 @@
 <div class="go-top-container">
-  <div class="nav-toggle"></div>
-  <div class="go-top"></div>
+  <div class="percent"></div>
+  <div class="go-top-inner1"></div>
+  <div class="go-top-inner2"></div>
+  <div class="rocket iconfont">&#xe612;</div>
 </div>
